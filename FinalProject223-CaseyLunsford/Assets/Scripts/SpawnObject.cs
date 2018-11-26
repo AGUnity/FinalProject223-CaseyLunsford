fd6ed45929f4148ae9f4836f35199a1de72b6aed
CLUnity <44986121+CLUnity@users.noreply.github.com>
2018-11-25 20:04:51 -0500

    InitialStartofProject
    Working Primarily on building the level and adding some of the neccessary animation will continue tomorrow.

---
+++ FinalProject223-CaseyLunsford/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public void OnTriggerEnter()
+    {
+        GameObject Prefab = Instantiate(Prefab, transform.position) ;
+    }
+}
+
