fad039c365f68d660f7db8ce903afe08494288c5
CLUnity <44986121+CLUnity@users.noreply.github.com>
2018-11-30 22:41:11 -0500

    LastFewChanges
    Before finishing this side project with animation focus wanted to fit in the last few and will work on the rest to finish tommorow.

--- FinalProject223-CaseyLunsford/Assets/Scripts/ColorChanger.cs
+++ FinalProject223-CaseyLunsford/Assets/Scripts/ColorChanger.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ColorChanger : MonoBehaviour {
-    public Renderer objectRender = null;
-	public void OnTriggerEnter () {
-        objectRender = GetComponent<Renderer>();
-        objectRender.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
+    private void Start()
+    {
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
     }
 }
---
+++ FinalProject223-CaseyLunsford/Assets/Scripts/SizeChangeScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeChangeScript : MonoBehaviour {
+
+    public Animator TargetAnimator = null;
+
+    public void OnTriggerEnter()
+    {
+        TargetAnimator.enabled = true;
+    }
+}
